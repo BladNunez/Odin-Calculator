5fd1f9131c31870774a8db3ce617f94c0e4e1614
BladNunez <Nunez.Bladimir@yahoo.com>
2023-03-03 14:44:34 -0500

    create multiply and divide functions on 2 numbers

--- calculator.js
+++ calculator.js
@@ -12,5 +12,24 @@ function sub(numOne,numTwo){
         return num;
 }
 
+//Divide function
+function divide(numOne,numTwo){
+        if(numOne === 0 || numTwo === 0){
+                console.log("Can not divide by zero!");
+        }
+
+        let num = numOne / numTwo;
+
+        return num
+}
+
+function multiply(numOne,numTwo){
+        let num = numOne * numTwo;
+
+        return num;
+}
+
+
+
 
 
