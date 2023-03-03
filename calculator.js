a793af15264ea44e42a97ddcc90fd066ae03cbf8
BladNunez <Nunez.Bladimir@yahoo.com>
2023-03-03 14:41:08 -0500

    Create add and subtraction functions

---
+++ calculator.js
@@ -0,0 +1,16 @@
+//Add function 
+function add(numOne,numTwo){
+        let num = numOne + numTwo;
+
+        return num
+}
+
+//Sub function 
+function sub(numOne,numTwo){
+        let num = numOne - numTwo;
+
+        return num;
+}
+
+
+
