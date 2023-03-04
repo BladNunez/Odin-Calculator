6413bc4ec8bc72387a5b50f19125d575d21e101a
BladNunez <Nunez.Bladimir@yahoo.com>
2023-03-03 19:39:40 -0500

    create switch to display number when button is clicked on display div

--- calculator.js
+++ calculator.js
@@ -48,13 +48,45 @@ function operate(sign,numOne,numTwo){
 
 
 //add eventlisteners on all number buttons
-let display = document.getElementsByClassName("display");
+let display = document.querySelector('.display');
+
 
 const buttons = document.querySelectorAll("button")
 buttons.forEach(button => {
   button.addEventListener("click", () => {
-       console.log("Worked");
+        let b = button.value;
 
-    
+        switch(b){
+                case '7':
+                        display.append(7);
+                        break;
+                case '8':
+                        display.append(8);
+                        break;
+                case '9':
+                         display.append(9);
+                         break;
+                 case '4':
+                        display.append(4);
+                        break;
+                case '5':
+                        display.append(5);
+                        break;
+                case '6':
+                        display.append(6);
+                        break;
+                case '1':
+                        display.append(1);
+                        break;
+                case '2':
+                        display.append(2);
+                        break;
+                case '3':
+                        display.append(3);
+                        break;
+                case '0':
+                        display.append(0);
+                        break;
+        }
   });
   });
